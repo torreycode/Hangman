37ff630553b4ddc380a7a4972b5048bbe39ccf72
Torrey Thomas <Torreyisgone@gmail.com>
2019-10-31 12:54:01 -0400

    Created an array of words that is read and a word is picked randomly from the array

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("I'm alive");
+const words = ['apples','bat','blackcat','boo','candycorn','costumes','ghost','goblin','halloween','hauntedhouse','jackolantern','mask','moon','mummy','night','october','pirate','princess','pumpkin','spider','spooky','superhero','trickortreat','vampire','witch'];
+
+let randWord = Math.floor(Math.random() * words.length); //Math.floor is rounding off the number of words in the words array to the nearest integer.
+let chosenWord = words[randWord]; // Picking a random word from the words array.
+let underScore = []; // empty array holding underscores
+console.log(chosenWord);
+
+
+let userUnderscore = () => {
+    for(let i=0; i < chosenWord.length; i++){
+
+    }
+}
