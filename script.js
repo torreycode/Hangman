fe9c301695eb67ae58e855f018c90dd3e6110b6c
Torrey Thomas <Torreyisgone@gmail.com>
2019-11-04 18:25:37 -0500

    got the game restart to work, somehow, someway

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 
+//game reset logic
+
+function reset()
+{
 
 //Contains all the spookiest vocabulary known to the human kind.
-const words = ['bat', 'boo', 'costume', 'ghost', 'goblin', 'haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
+let words = ['bat', 'boo', 'costume', 'ghost', 'goblin', 'haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
+randWord = Math.floor(Math.random() * words.length); //Math.floor is rounding off the number of words in the words array to the nearest integer.
+ chosenWord = words[randWord]; // Picking a random word from the words array.
+ guessesLeft = 5;
+ underScore = []; // empty array holding underscores
+rightWords = []; //array holding the correct guesses.
+ wrongWords = []; // array holding incorrect guesses.
+console.log(chosenWord);
+lettersInWord = chosenWord.split('');
+//Get the number of blanks
+numBlanks = lettersInWord.length;
+console.log(lettersInWord);
+
+startGame();
+}
+
+
+function startGame(){
+//Contains all the spookiest vocabulary known to the human kind.
+const words = ['bat', 'costume', 'ghost', 'goblin', 'haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
 let randWord = Math.floor(Math.random() * words.length); //Math.floor is rounding off the number of words in the words array to the nearest integer.
 let chosenWord = words[randWord]; // Picking a random word from the words array.
 let guessesLeft = 5;
@@ -10,6 +33,10 @@ let rightWords = []; //array holding the correct guesses.
 let wrongWords = []; // array holding incorrect guesses.
 console.log(chosenWord);
 
+lettersInWord = chosenWord.split('');
+//Get the number of blanks
+numBlanks = lettersInWord.length;
+console.log(lettersInWord);
 
 // Dom Manipulation
 
@@ -26,7 +53,7 @@ console.log(docUnderScore);
 
 // The expression below generates an array of underscores equivalent to the amount of characters in the randomly generated word.
 let userUnderscore = () => {
-    for (let i = 0; i < chosenWord.length; i++) {
+    for (let i = 0; i < lettersInWord.length; i++) {
         underScore.push('_');
     }
     return underScore;
@@ -34,36 +61,36 @@ let userUnderscore = () => {
 
 //testing
 console.log(userUnderscore());
-console.log(chosenWord);
-let splitWord = chosenWord.split(" ");``
-console.log(splitWord);
+console.log(lettersInWord);
+
 
 document.addEventListener('keypress', (event) => {   // the event listener is listening to each key that is pressed and displaying that letter associated in the console using fromCharCode and event.keyCode
     let keyword = String.fromCharCode(event.keyCode);
     console.log(keyword);
-    // if users guess is right
-    underScore[chosenWord.indexOf(keyword)] = keyword; //find the chosenWord and in the code below places the equivalent # of underscores inside of the text/guesses box.
-    docUnderScore[0].innerHTML = underScore.join(' ')
-    docrightguess[0].innerHTML = rightWords.join(' '); // checks if input is right
-    docwrongguess[0].innerHTML = wrongWords.join(' ');
-  
-    // checks if input is wrong
-    if (chosenWord.indexOf(keyword) > -1) {
-        // push/add to the right words array.
+    underScore[lettersInWord.indexOf(keyword)] = keyword; //find the chosenWord and in the code below places the equivalent # of underscores inside of the text/guesses box.
+    docUnderScore[0].innerHTML = underScore.join('')
+    docrightguess[0].innerHTML = rightWords.join(''); // checks if input is right
+    docwrongguess[0].innerHTML = wrongWords.join('');
+
+
+    if (lettersInWord.indexOf(keyword) > -1) {
         rightWords.push(keyword);
         if (underScore.join('') === chosenWord) {
-            alert("You Won");
-            reset();
+           alert("You win")
+           reset();
         }
 
 
     }
     else if (wrongWords.length >= 5) {
-        guessesLeft - 1;
-        alert("You've Lost the Game");
+        alert("You lost");
         reset();
 
     }
-    else { wrongWords.push(keyword) };
-});
+    else {
+         wrongWords.push(keyword) 
+         console.log("guess was wrong")
+        };
+})};
 
+ startGame();
