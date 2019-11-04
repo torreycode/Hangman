b5a70c8f518434bc52bc558e8e8984feaa876ce2
Torrey Thomas <Torreyisgone@gmail.com>
2019-11-04 16:46:31 -0500

    added miscellaneous changes to the hangman functionality, as well as some css edits

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-// Global Variables
-//Allows a letter to be used more than once.
-var doubleWord = ['a','b','c', 'd','e','f', 'g','h','i','j','k','l', 'm','n','o','p','q','r', 's','t','u', 'v','w','x','y','z'];
 
 
 //Contains all the spookiest vocabulary known to the human kind.
-const words = ['apples','bat','blackcat','boo','candycorn','costumes','ghost','goblin','halloween','hauntedhouse','jackolantern','mask','moon','mummy','night','october','pirate','princess','pumpkin','spider','spooky','superhero','trickortreat','vampire','witch'];
+const words = ['bat', 'boo', 'costume', 'ghost', 'goblin', 'haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
 let randWord = Math.floor(Math.random() * words.length); //Math.floor is rounding off the number of words in the words array to the nearest integer.
 let chosenWord = words[randWord]; // Picking a random word from the words array.
+let guessesLeft = 5;
 let underScore = []; // empty array holding underscores
 let rightWords = []; //array holding the correct guesses.
 let wrongWords = []; // array holding incorrect guesses.
@@ -15,47 +13,57 @@ console.log(chosenWord);
 
 // Dom Manipulation
 
-let docUnderScore = document.getElementsByClassName('underscore');
+let docUnderScore = document.querySelectorAll(".underscore");
+let docrightguess = document.querySelectorAll(".rightguess");
+let docwrongguess = document.querySelectorAll(".wrongguess");
 
+console.log(docwrongguess);
+console.log(docrightguess);
+console.log(docUnderScore);
 
 
 // Main Variables
 
 // The expression below generates an array of underscores equivalent to the amount of characters in the randomly generated word.
-let userUnderscore = () => {   
-    for(let i=0; i < chosenWord.length; i++){
-underScore.push('_');
-
+let userUnderscore = () => {
+    for (let i = 0; i < chosenWord.length; i++) {
+        underScore.push('_');
     }
     return underScore;
 }
+
 //testing
 console.log(userUnderscore());
 console.log(chosenWord);
+let splitWord = chosenWord.split(" ");``
+console.log(splitWord);
 
 document.addEventListener('keypress', (event) => {   // the event listener is listening to each key that is pressed and displaying that letter associated in the console using fromCharCode and event.keyCode
     let keyword = String.fromCharCode(event.keyCode);
     console.log(keyword);
     // if users guess is right
-    if(chosenWord.indexOf(keyword) > -1){
-    // push/add to the right words array.
-    rightWords.push(keyword);
-    docUnderScore;
+    underScore[chosenWord.indexOf(keyword)] = keyword; //find the chosenWord and in the code below places the equivalent # of underscores inside of the text/guesses box.
+    docUnderScore[0].innerHTML = underScore.join(' ')
+    docrightguess[0].innerHTML = rightWords.join(' '); // checks if input is right
+    docwrongguess[0].innerHTML = wrongWords.join(' ');
+  
+    // checks if input is wrong
+    if (chosenWord.indexOf(keyword) > -1) {
+        // push/add to the right words array.
+        rightWords.push(keyword);
+        if (underScore.join('') === chosenWord) {
+            alert("You Won");
+            reset();
+        }
 
 
-    // place underscore with correct letter
-    underScore[chosenWord.indexOf(keyword)] =  keyword;
-    docUnderScore.innerHTML = underScore.join(' ');
-    doc
-    if (underScore.join('') === chosenWord){
-        alert("You Won");
     }
-    console.log(underScore);
- 
+    else if (wrongWords.length >= 5) {
+        guessesLeft - 1;
+        alert("You've Lost the Game");
+        reset();
+
     }
-    else{wrongWords.push(keyword)};
+    else { wrongWords.push(keyword) };
 });
-
-
-docUnderScore[0].innerHTML = userUnderscore().join(' ')  // reading the innerHTML and replacing it with the appropriate amount of underscores, corresponding to the tezt in the console.log;
 
