e8fb149b32250c0a74c03d9833f285addeb94edc
Torrey Thomas <Torreyisgone@gmail.com>
2019-11-06 00:32:06 -0500

    dynamically updates the html and shows if the guess is right

--- script.js
+++ script.js
@@ -11,20 +11,24 @@ function startGame(){
 
     
 //Contains all the spookiest vocabulary known to the human kind.
-const words = ['bat', 'costume', 'ghost', 'goblin', 'haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
+const words = ['bat', 'costume', 'ghost', 'goblin','haunted', 'mask', 'night', 'pirate', 'spider', 'hero', 'vampire', 'witch'];
 
 //important functionality variables
 let randWord = Math.floor(Math.random() * words.length); //Math.floor is rounding off the number of words in the words array to the nearest integer.
 let chosenWord = words[randWord]; // Picking a random word from the words array.
+console.log(chosenWord);
 let underScore = []; // empty array holding underscores
 let rightWords = []; //array holding the correct guesses.
+console.log(rightWords);
 let wrongWords = []; // array holding incorrect guesses.
 let guessesLeft = 8; // beginning amount of guesses left
 let lettersInWord = chosenWord.split(''); // splits the chosenWord in an array. Can be looped through.
 let numBlanks = lettersInWord.length; // This dynamically gets the number of blanks in the split version of chosen word.
 let docUnderScore = document.getElementById("underscore"); // gets the html element underscore
-let docrightguess = document.querySelectorAll(".rightguess"); // gets the right guess html element
+let docrightguess = document.getElementById("rightguess"); // gets the right guess html element
 let docwrongguess = document.querySelectorAll(".wrongguess"); // gets the wrong guess html element
+console.log(wrongWords);
+console.log(lettersInWord);
 
 
 // dynamically generates the correct amount of underscores in the chosen word.
@@ -35,34 +39,49 @@ let hangWord = () => {
         return underScore;
     }
 hangWord();
-    
+console.log(hangWord);
+console.log(underScore);
+
 docUnderScore.innerHTML = underScore;
 
-// testing 
-console.log(hangWord);
-console.log(chosenWord);
-console.log(rightWords);
-console.log(underScore);
-console.log(wrongWords);
-console.log(lettersInWord);
 
-// listening for keypresses
+
+
+  // listening for keypresses
 document.addEventListener('keypress', (event) => {   // the event listener is listening to each key that is pressed and displaying that letter associated in the console using fromCharCode and event.keyCode
     let keyword = String.fromCharCode(event.keyCode);
     console.log(keyword);
 
-for (i=0; i < lettersInWord.length; i++){
-   if(lettersInWord.indexOf(keyword)){
-       console.log("you guess was correct")
-   }
-}});
+
+    // looping through each syllable and checking if it's correct or not.
+lettersInWord.forEach(function(element) {
+
+    underScore[lettersInWord.indexOf(keyword)] = keyword; 
+    docUnderScore.innerHTML = underScore.join(' ');
+    docrightguess.innerHTML = rightWords.join(' ');
+    docwrongguess.innerHTML = wrongWords.join(' ');
+
+    if(keyword === element){
+        rightWords.push(keyword);
+        console.log('you\'ve guessed correctly')
+    }
+  });
+
+  if(underScore.join('') === chosenWord){
+    alert('you win');
+}
+})
 
 
 
-
-
+  
 
 };
+
+
+
+
+
 
 startGame();
 
@@ -81,9 +100,7 @@ startGame();
 
 
 
-// document.addEventListener('keypress', (event) => {   // the event listener is listening to each key that is pressed and displaying that letter associated in the console using fromCharCode and event.keyCode
-//     let keyword = String.fromCharCode(event.keyCode);
-//     console.log(keyword);
+
 //     underScore[lettersInWord.indexOf(keyword)] = keyword; //find the chosenWord and in the code below places the equivalent # of underscores inside of the text/guesses box.
 //     docUnderScore[0].innerHTML = underScore.join(' ')
 //     docrightguess[0].innerHTML = rightWords.join(' '); // checks if input is right
